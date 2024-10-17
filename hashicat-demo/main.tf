6564ac45ff25844990e16fb064d16b307eef042c
cesteban29 <cesteban@hashicorp.com>
2024-10-16 17:23:11 -0700

    upload to github

---
+++ hashicat-demo/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0.54.0"
+    }
+    
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.30.0"
+    }
+  }
+}
+
+provider "hcp" {}
+
+provider "aws" {
+  region = var.region
+}
+
+#PACKER ITERATION
+data "hcp_packer_iteration" "hashicat" {
+  bucket_name = "hashicat-demo"
+  channel     = "latest"
+}
+
+#PACKER IMAGE
+data "hcp_packer_image" "ubuntu_us_east_1" {
+  bucket_name    = "hashicat-demo"
+  cloud_provider = "aws"
+  iteration_id   = data.hcp_packer_iteration.hashicat.ulid
+  region         = "us-east-1"
+}
+
+module "hashicat" {
+  source  = "app.terraform.io/cesteban-tfc/hashicat/aws"
+  version = "1.9.1"
+  instance_type = "var.instance_type"
+  region = var.region
+  instance_ami = data.hcp_packer_image.ubuntu_us_east_1.cloud_image_id
+}
---
+++ hashicat-demo/packer-image/files/deploy_app.sh
@@ -0,0 +1,15 @@
+<html>
+  <head><title>Meow!</title></head>
+  <body>
+  <div style="width:800px;margin: 0 auto">
+
+  <!-- BEGIN -->
+  <center><img src="http://placekitten.com/600/400"></img></center>
+  <center><h1>Meow World!</h1></center>
+  <center><h2>Welcome to Carlos' Cat app!</h2></center>
+  <!-- END -->
+
+  </div>
+  </body>
+</html>
+
