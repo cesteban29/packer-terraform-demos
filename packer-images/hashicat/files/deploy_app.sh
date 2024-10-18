584d1cb785174d82fdeb9f1c8ad678671b0513a7
cesteban29 <cesteban@hashicorp.com>
2024-10-18 08:42:47 -0700

    version 7

--- packer-images/hashicat/files/deploy_app.sh
+++ packer-images/hashicat/files/deploy_app.sh
@@ -7,7 +7,7 @@
   <center><img src="https://placecats.com/600/400?fit=contain&position=top"></img></center>
   <center><h1>Meow World!</h1></center>
   <center><h2>Welcome to Carlos' Cat app</h2></center>
-  <center><h3>This is Version 6 of HashiCat!</h3></center>
+  <center><h3>This is Version 7 of HashiCat!</h3></center>
   <!-- END -->
 
   </div>
