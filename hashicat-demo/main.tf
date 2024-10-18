e40515ffa3b046910d934f47788255d8aec4fda0
cesteban29 <cesteban@hashicorp.com>
2024-10-17 22:47:00 -0700

    added comments

--- hashicat-demo/main.tf
+++ hashicat-demo/main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     hcp = {
       source  = "hashicorp/hcp"
-      version = "~> 0.54.0"
+      version = "~> 0.97.0"
     }
     
     aws = {
       source = "hashicorp/aws"
-      version = "4.30.0"
+      version = "5.72.1"
     }
   }
 }
@@ -18,24 +18,27 @@ provider "aws" {
   region = var.region
 }
 
-#PACKER ITERATION
-data "hcp_packer_iteration" "hashicat" {
+#PACKER VERSION
+# Get the latest version of the Packer build from HCP Packer
+data "hcp_packer_version" "hashicat" {
   bucket_name = "hashicat-demo"
-  channel     = "latest"
+  channel_name = "latest"
 }
 
-#PACKER IMAGE
-data "hcp_packer_image" "ubuntu_us_east_1" {
+#PACKER ARTIFACT
+# Get the AMI ID from the HCP Packer registry
+data "hcp_packer_artifact" "hashicat_us_east_1" {
   bucket_name    = "hashicat-demo"
-  cloud_provider = "aws"
-  iteration_id   = data.hcp_packer_iteration.hashicat.ulid
+  version_fingerprint = data.hcp_packer_version.hashicat.fingerprint
+  platform = "aws"
   region         = "us-east-1"
 }
 
+# HCP TERRAFORM MODULE
 module "hashicat" {
-  source  = "app.terraform.io/cesteban-tfc/hashicat/aws"
+  source  = "app.terraform.io/CE-Demos/hashicat/aws"
   version = "1.9.1"
   instance_type = "var.instance_type"
-  region = var.region
-  instance_ami = data.hcp_packer_image.ubuntu_us_east_1.cloud_image_id
+  region = var.region 
+  instance_ami = data.hcp_packer_artifact.hashicat_us_east_1.external_identifier # AMI ID from HCP Packer registry
 }
