c3dc017522f6cc1e50f0b8b977d2c5ece307666b
cesteban29 <cesteban@hashicorp.com>
2024-10-18 08:17:19 -0700

    version 5 of image

--- packer-images/hashicat/files/deploy_app.sh
+++ packer-images/hashicat/files/deploy_app.sh
@@ -4,9 +4,10 @@
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://placekitten.com/600/400"></img></center>
+  <center><img src="https://placecats.com/600/400?fit=contain&position=top"></img></center>
   <center><h1>Meow World!</h1></center>
-  <center><h2>Welcome to Carlos' Cat app!</h2></center>
+  <center><h2>Welcome to Carlos' Cat app</h2></center>
+  <center><h3>This is Version 5 of HashiCat!</h3></center>
   <!-- END -->
 
   </div>
