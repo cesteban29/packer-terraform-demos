366356595f06e949b4339a9c321f5c331abb8453
cesteban29 <cesteban@hashicorp.com>
2024-10-18 08:28:07 -0700

    added dev and prod folders within hashicat

---
+++ hashicat-demo/prod/check.tf
@@ -0,0 +1,23 @@
+check "health_check" {
+  data "http" "hashicat_web" {
+    url = module.hashicat.catapp_url
+  }
+
+  assert {
+    condition = data.http.hashicat_web.status_code == 200
+    error_message = "${data.http.hashicat_web.url} returned an unhealthy status code"
+  }
+}
+
+check "ami_version_check" {
+  data "aws_instance" "hashicat_current" {
+    instance_tags = {
+      Name = "${var.prefix}-hashicat-instance"
+    }
+  }
+
+  assert {
+    condition = data.aws_instance.hashicat_current.ami == data.hcp_packer_artifact.hashicat_us_east_1.external_identifier
+    error_message = "Must use the latest available AMI, ${data.hcp_packer_artifact.hashicat_us_east_1.external_identifier}."
+  }
+}
---
+++ hashicat-demo/prod/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0.97.0"
+    }
+    
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.72.1"
+    }
+  }
+}
+
+provider "hcp" {
+  project_id = var.hcp_project_id
+}
+
+provider "aws" {
+  region = var.region
+}
+
+#PACKER VERSION
+# Get the latest version of the Packer build from HCP Packer
+data "hcp_packer_version" "hashicat" {
+  bucket_name = "hashicat-demo"
+  channel_name = "prod"
+}
+
+#PACKER ARTIFACT
+# Get the AMI ID from the HCP Packer registry
+data "hcp_packer_artifact" "hashicat_us_east_1" {
+  bucket_name    = "hashicat-demo"
+  version_fingerprint = data.hcp_packer_version.hashicat.fingerprint
+  platform = "aws"
+  region         = "us-east-1"
+}
+
+# HCP TERRAFORM MODULE
+module "hashicat" {
+  source  = "app.terraform.io/cesteban-tfc/hashicat/aws"
+  version = "1.9.3"
+  instance_type = var.instance_type
+  region = var.region 
+  prefix = var.prefix
+  instance_ami = data.hcp_packer_artifact.hashicat_us_east_1.external_identifier # AMI ID from HCP Packer registry
+}
