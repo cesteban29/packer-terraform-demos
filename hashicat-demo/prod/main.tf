99ef785b7a05b94d2fec5755d63035541eb05265
cesteban29 <cesteban@hashicorp.com>
2024-11-07 12:17:08 -0800

    updated hashicat prod

--- hashicat-demo/prod/main.tf
+++ hashicat-demo/prod/main.tf
@@ -1,47 +1,27 @@
-terraform {
-  required_providers {
-    hcp = {
-      source  = "hashicorp/hcp"
-      version = "~> 0.97.0"
-    }
-    
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.72.1"
-    }
-  }
-}
+#main.tf
 
-provider "hcp" {
-  project_id = var.hcp_project_id
-}
-
-provider "aws" {
-  region = var.region
-}
-
-#PACKER VERSION
+# DYNAMIC PACKER IMAGE RETRIEVAL - GOLDEN IMAGE PIPELINE
 # Get the latest version of the Packer build from HCP Packer
-data "hcp_packer_version" "hashicat" {
+data "hcp_packer_version" "hashicat-version" {
   bucket_name = "hashicat-demo"
   channel_name = "prod"
 }
 
-#PACKER ARTIFACT
 # Get the AMI ID from the HCP Packer registry
-data "hcp_packer_artifact" "hashicat_us_east_1" {
+data "hcp_packer_artifact" "hashicat-artifact" {
   bucket_name    = "hashicat-demo"
-  version_fingerprint = data.hcp_packer_version.hashicat.fingerprint
+  version_fingerprint = data.hcp_packer_version.hashicat-version.fingerprint
   platform = "aws"
-  region         = "us-east-1"
+  region         = var.region
 }
 
-# HCP TERRAFORM MODULE
+# Deploys dynamically retrieved AMI from HCP Packer registry to AWS EC2
 module "hashicat" {
-  source  = "app.terraform.io/cesteban-tfc/hashicat/aws"
-  version = "1.9.3"
+  # EC2 INSTANCE MODULE
+  source  = "app.terraform.io/cesteban-tfc/ec2-instance/aws"
+  version = "1.5.0"
   instance_type = var.instance_type
   region = var.region 
   prefix = var.prefix
-  instance_ami = data.hcp_packer_artifact.hashicat_us_east_1.external_identifier # AMI ID from HCP Packer registry
+  instance_ami = data.hcp_packer_artifact.hashicat-artifact.external_identifier # AMI ID from HCP Packer registry
 }
--- hashicat-demo/prod/output.tf
+++ hashicat-demo/prod/output.tf
@@ -1,8 +1,7 @@
-
 output "catapp_url"{
-  value = module.hashicat.catapp_url
+  value = module.hashicat.instance_url
 }
   
 output "catapp_ip"{
-  value = module.hashicat.catapp_ip
+  value = module.hashicat.instance_ip
 }
